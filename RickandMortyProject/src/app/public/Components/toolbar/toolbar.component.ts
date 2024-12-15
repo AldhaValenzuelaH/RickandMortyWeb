dd75aa6059eb4861c83884785c8cea76909669eb
Aldhair Johan Juan Valenzuela Huillcaya <149197456+AldhaValenzuelaH@users.noreply.github.com>
2024-12-14 23:42:08 -0500

    feat: add switch language

--- RickandMortyProject/src/app/public/Components/toolbar/toolbar.component.ts
+++ RickandMortyProject/src/app/public/Components/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
 import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatSlideToggle} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,11 +10,20 @@ import {MatButton, MatIconButton} from '@angular/material/button';
     MatToolbar,
     MatIcon,
     MatIconButton,
-    MatButton
+    MatButton,
+    MatSlideToggle
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
+  currentLanguage: string = 'en';
 
+  toggleLanguage(event: any): void {
+    this.currentLanguage = event.checked ? 'es' : 'en';
+    console.log(`Language switched to: ${this.currentLanguage}`);
+    // Aquí puedes agregar lógica adicional para cambiar dinámicamente el idioma
+  }
 }
+
+
